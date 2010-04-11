bbf993d55b1c5fe0ef5c37425bb8cd706f7dba6a
Fabian Topfstedt <topfstedt@schneevonmorgen.com>
2010-04-11 03:47:55 -0700

    send mail to creators of flagged content once it got flagged

--- flag/listeners.py
+++ flag/listeners.py
@@ -1,3 +1,39 @@
-def start_listening():
-    pass
+from django.conf import settings
+from django.core.mail import send_mail
+from django.db.models import get_model
+from django.db.models.signals import post_save
 
+EMAIL_NOTIFY_CREATOR = getattr('settings', 'FLAG_EMAIL_NOTIFY_CREATOR', True) 
+
+
+def _post_create_email_notify_creator(sender, instance, created, **kwargs):
+    """
+    Sends a new flag notification email to the creator of a flagged item.
+    """
+    if created:
+        accused = instance.flagged_content.creator
+        accusation = instance.comment
+        flagged_object = unicode(instance.flagged_content.content_object)
+
+        subject = """"%s" got flagged as inappropriate""" % flagged_object
+        message = \
+"""
+Dear %s,
+
+we received an accusation concerning your content. If you share the accusation,
+please change or delete your content so that everyone is happy again. Our staff
+also got informed and will check if you stuck to our terms of use.
+
+Content: "%s"
+Accusation: "%s"
+
+""" % (accused, flagged_object, accusation)
+
+        accused.send_mail(subject, message)
+
+
+def start_listening():
+    flag_instance_model = get_model('flag', 'FlagInstance')
+
+    if flag_instance_model and EMAIL_NOTIFY_CREATOR:
+        post_save.connect(_post_create_email_notify_creator, sender=flag_instance_model)
